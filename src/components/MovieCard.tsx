1320e8be3a3b815e1a0e043ad6dea054964a64fb
GiacomoInvernizzi <giacomoinvernizzi@hotmail.com>
2024-01-30 20:04:28 +0100

    fix: creata grid per le card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -15,19 +15,24 @@ import { MovieType } from "../types/types";
 
 export const MovieCard = (movie: MovieType) => {
   return (
-    <Card maxW="md">
+    <Card maxW="md" bg="white" boxShadow="md" borderRadius="lg">
       <CardHeader>
-        <Flex>
-          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-            <Box>
-              <Heading size="sm">{movie.Title}</Heading>
-              <Text>{movie.Year}</Text>
-            </Box>
-          </Flex>
+        <Flex justify="space-between" align="center">
+          <Box>
+            <Heading size="md">{movie.Title}</Heading>
+            <Text color="gray.500">{movie.Year}</Text>
+          </Box>
         </Flex>
       </CardHeader>
-      <CardBody>
-        <Image objectFit="cover" src={movie.Poster} alt={movie.Title} />
+      <CardBody p="4">
+        <Image
+          objectFit="cover"
+          src={movie.Poster}
+          alt={movie.Title}
+          borderRadius="md"
+          alignSelf={"center"}
+          height={"100%"}
+        />
       </CardBody>
       <CardFooter
         justify="space-between"
@@ -38,12 +43,24 @@ export const MovieCard = (movie: MovieType) => {
           },
         }}
       >
-        <Button flex="1" variant="ghost" leftIcon={<TriangleUpIcon />}>
-          Like
-        </Button>
-        <Button flex="1" variant="ghost" leftIcon={<TriangleDownIcon />}>
-          Dislike
-        </Button>
+        <Flex justify="center">
+          <Button
+            flex="1"
+            variant="ghost"
+            leftIcon={<TriangleUpIcon />}
+            _hover={{ bg: "gray.200" }}
+          >
+            Like
+          </Button>
+          <Button
+            flex="1"
+            variant="ghost"
+            leftIcon={<TriangleDownIcon />}
+            _hover={{ bg: "gray.200" }}
+          >
+            Dislike
+          </Button>
+        </Flex>
       </CardFooter>
     </Card>
   );
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,24 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, SimpleGrid, Stack } from "@chakra-ui/react";
 import { MovieType } from "../types/types";
 import { MovieCard } from "./MovieCard";
 
 type MovieListProps = {
-    movies: MovieType[],
-}
+  movies: MovieType[];
+};
 
 export const MovieList = ({ movies }: MovieListProps) => {
   return (
     <Box>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.imdbID}
-          Title={movie.Title}
-          Year={movie.Year}
-          imdbID={movie.imdbID}
-          Type={movie.Type}
-          Poster={movie.Poster}
-        />
-      ))}
+      <SimpleGrid columns={[null, null, 3]} spacing={'40px'}>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            Title={movie.Title}
+            Year={movie.Year}
+            imdbID={movie.imdbID}
+            Type={movie.Type}
+            Poster={movie.Poster}
+          />
+        ))}
+      </SimpleGrid>
     </Box>
   );
 };
